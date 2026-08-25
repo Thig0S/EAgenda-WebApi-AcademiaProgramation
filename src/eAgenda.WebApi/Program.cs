ebc9a2095fb3552e07385b74ddf8f2b816f12bb0
thig0S_ <thiagokovalski5@gmail.com>
2026-08-25 15:00:14 -0300

    add injecao de dependencias infra e services

--- src/eAgenda.WebApi/Program.cs
+++ src/eAgenda.WebApi/Program.cs
@@ -1,5 +1,24 @@
+using eAgenda.Aplicacao;
+using eAgenda.Infra;
+using eAgenda.Infra.Compartilhado.Orm;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddApplicationServices();
+builder.Services.AddInfraRepositories(builder.Configuration, builder.Logging);
+builder.Services.AddControllers();
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    using var scope = app.Services.CreateScope();
+
+    var dbcontext = scope.ServiceProvider.GetRequiredService<EAgendaDbContext>();
+
+    dbcontext.Database.Migrate();
+}
 
 app.UseHttpsRedirection();
 app.MapControllers();
