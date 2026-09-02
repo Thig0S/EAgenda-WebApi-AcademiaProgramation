91087907aed9b163c1731ed7ca496b860a3c6641
thig0S_ <thiagokovalski5@gmail.com>
2026-09-02 15:08:43 -0300

    add Identity a infra

--- src/eAgenda.Infra/InjecaoDependencia.cs
+++ src/eAgenda.Infra/InjecaoDependencia.cs
@@ -11,6 +11,7 @@ using eAgenda.Infra.Modulos.ModuloCompromisso;
 using eAgenda.Infra.Modulos.ModuloContato;
 using eAgenda.Infra.Modulos.ModuloDespesa;
 using eAgenda.Infra.Modulos.ModuloTarefa;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -39,18 +40,36 @@ public static class InjecaoDependencia
                );
            }
 
-            if (configuration["Infra:DatabaseProvider"] == "InMemory")
-            {
-                options.UseInMemoryDatabase("eAgenda");
-            }
-            else
-            {
-                options.UseSqlServer(connectionString, opt =>
-                {
-                    opt.EnableRetryOnFailure(3);
-                });
-            }
+           if (configuration["Infra:DatabaseProvider"] == "InMemory")
+           {
+               options.UseInMemoryDatabase("eAgenda");
+           }
+           else
+           {
+               options.UseSqlServer(connectionString, opt =>
+               {
+                   opt.EnableRetryOnFailure(3);
+               });
+           }
        });
+
+        services.AddIdentityCore<IdentityUser<Guid>>(
+            options =>
+            {
+                options.User.RequireUniqueEmail = true;
+                options.SignIn.RequireConfirmedEmail = false;
+                options.Password.RequiredLength = 8;
+                options.Password.RequireNonAlphanumeric = true;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireLowercase = false;
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+                options.Lockout.MaxFailedAccessAttempts = 5;
+                options.Lockout.AllowedForNewUsers = true;
+            }
+        ).AddRoles<IdentityRole<Guid>>()
+        .AddEntityFrameworkStores<EAgendaDbContext>()
+        .AddSignInManager()
+        .AddDefaultTokenProviders();
 
         services.AddScoped<IRepositorioContato, RepositorioContatoEmOrm>();
         services.AddScoped<IRepositorioCompromisso, RepositorioCompromissoEmOrm>();
